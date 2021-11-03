a20c77168bbb3f37949e3698886164c4b0e46739
18016406 <zceetsl@ucl.ac.uk>
2021-11-03 22:57:23 +0000

    Added testing code to resize images to normalise image size after trimming

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import glob
 import func as myf
 import skimage.io as si
+from skimage.transform import resize
 import sklearn as sl
 
-imagelist = glob.glob('testimport/*.jpg')
-images = np.array([si.imread(image, as_gray=True) for image in imagelist])
+imagelist = glob.glob('testimport/*.jpg')  # Creates list of names of JPEG files in specified folder
+images = np.array(
+    [si.imread(image, as_gray=True) for image in imagelist])  # import images, ensuring grayscale, into a 3D ndarray
 
-# si.imshow(trimmed)
-# si.show()
+print('images array shape: ', images.shape)
+print('No. of images: ', len(imagelist))
+
+trimmedimages = []
+for i in range(0, len(imagelist)):
+    trimmedimages.append(myf.trim0rows(images[i][:][:]))  # Creates a list of the imported images after being cropped
+
+testimg = trimmedimages[0]
+print('test image shape: ', testimg.shape)
+
+segmentedimg = resize(testimg, np.array([150, 100]), anti_aliasing=True)    # Resizes images after trimming to normalize all sizes
+si.imshow(segmentedimg)
+si.show()
+
+print('resized image shape: ', segmentedimg.shape)
