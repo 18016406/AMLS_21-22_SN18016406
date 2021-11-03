03177697339e94e98975f43110099f7c45443e78
18016406 <zceetsl@ucl.ac.uk>
2021-11-03 19:23:16 +0000

    Added code in main.py to import images from a folder into an ndarray structure

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import glob
 import func as myf
 import skimage.io as si
 import sklearn as sl
 
-testimage = si.imread('image/IMAGE_0000.jpg', as_gray=True)
-trimmed = myf.trim0rows(testimage)
-print(trimmed[1,:])
-si.imshow(trimmed)
-si.show()
+imagelist = glob.glob('testimport/*.jpg')
+images = np.array([si.imread(image, as_gray=True) for image in imagelist])
+
+# si.imshow(trimmed)
+# si.show()
