252dce66645e8d700aba6873795ac77e4e12e086
18016406 <zceetsl@ucl.ac.uk>
2021-11-05 20:08:38 +0000

    Allowed selection of using the whole dataset in the program

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,5 @@
 
 9
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+<
+labelapp.csv,1\7\17fb347f16a3801998a59422038af2ef3aa78e14
--- main.py
+++ main.py
@@ -5,35 +5,15 @@ import skimage.io as si
 import csv
 from skimage.transform import resize
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+from sklearn.metrics import classification_report, accuracy_score
 
 ## IMPORTING IMAGES ##
-imagelist = glob.glob('testimport/*.jpg')  # Creates list of names of JPEG files in specified folder
+imagelist = glob.glob('image/*.jpg')  # Creates list of names of JPEG files in specified folder
 images = np.array(
     [si.imread(image, as_gray=True) for image in imagelist])  # import images, ensuring grayscale, into a 3D ndarray
 
 print('images array shape: ', images.shape)
 print('No. of images: ', len(imagelist))
-
-## TRIMMING AND SEGMENTING IMAGES ##
-trimmedimages = []
-for i in range(0, len(imagelist)):
-    trimmedimages.append(myf.trim0rows(images[i][:][:]))  # Creates a list of the imported images after being cropped
-
-segimg = []
-for j in trimmedimages:
-    segimg.append(
-        resize(j, np.array([150, 100]), anti_aliasing=True))  # Resizes images after trimming to normalize all sizes
-
-print('length of segmented images list: ', len(segimg))
-
-## CREATING ARRAY OF FEATURES FOR IMAGE POV DETECTION ##
-povfeatures = np.array([0, 0])
-for k in segimg:
-    tempmeans = np.array([np.mean(k[:, 0]), np.mean(k[-1, :])])
-    povfeatures = np.vstack([povfeatures, tempmeans])  # Makes an array of features, first column is mean of pic's left
-                                                # column values and second column is mean of pic's last row values
-povfeatures = np.delete(povfeatures, 0, 0)  # Removes the initialized value [0,0] at the top of features array
 
 ## READING LABELS ##
 csvfile = open('labelapp.csv')
@@ -48,14 +28,42 @@ for row in labelsfile:
     imgpov.append(row[2])
 csvfile.close()
 
+## TRIMMING AND SEGMENTING IMAGES ##
+trimmedimages = []
+for i in range(0, len(imagelist)):
+    trimmedimages.append(myf.trim0rows(images[i][:][:]))  # Creates a list of the imported images after being cropped
+
+segimg = []
+for j in trimmedimages:
+    segimg.append(
+        resize(j, np.array([150, 100]), anti_aliasing=True))  # Resizes images after trimming to normalize all sizes
+
+print('length of segmented images list: ', len(segimg))
+
+## CREATING ARRAY OF FEATURES FOR IMAGE POV DETECTION ##
+selectimgset = 1        #Select which img set to use in model (0 = before trimming/segmenting, 1 = after)
+povfeatures = np.array([0, 0])
+if selectimgset == 0:
+    for k in range(0, len(imagelist)):
+        selectedimage = images[k][:][:]
+        tempmeans = np.array([np.mean(selectedimage[:,5]), np.mean(selectedimage[-1, :])])
+        povfeatures = np.vstack([povfeatures, tempmeans])  # Makes an array of features, first column is mean of pic's 6th
+                                                        # column values and second column is mean of pic's last row values
+elif selectimgset == 1:
+    for k in segimg:
+        tempmeans = np.array([np.mean(k[:, 0]), np.mean(k[-1, :])])
+        povfeatures = np.vstack([povfeatures, tempmeans])  # Makes an array of features, first column is mean of pic's left
+                                                        # column values and second column is mean of pic's last row values
+povfeatures = np.delete(povfeatures, 0, 0)  # Removes the initialized value [0,0] at the top of features array
+
+
 ## *****FOR TESTING ONLY***** ##
-testlabelpov = imgpov[:1000]
+testlabelpov = imgpov[:len(imagelist)]                #Change according to sample size
 
 xtrain, xtest, ytrain, ytest = train_test_split(povfeatures, testlabelpov)
 ypredict = myf.LogisticRegressionPredict(xtrain,ytrain,xtest)
 print('Accuracy on test set: '+str(accuracy_score(ytest,ypredict)))
 print(classification_report(ytest,ypredict))
-print(confusion_matrix(ytest,ypredict))
 
 # si.imshow(segimg[1])
 # si.show()
