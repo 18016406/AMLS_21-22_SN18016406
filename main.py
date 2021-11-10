bf0b49d7fe47c949e43c2d58e778f393980fa01a
18016406 <zceetsl@ucl.ac.uk>
2021-11-10 16:37:55 +0000

    Focusing on task 1: classifying whether the presented image contains a tumor or not. Modified labels to suit this objective and created a feature vector

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from sklearn.experimental import enable_halving_search_cv
 from sklearn.model_selection import train_test_split, HalvingRandomSearchCV
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.ensemble import AdaBoostClassifier
-
+from skimage import feature
 ## IMPORTING IMAGES ##
 imagelist = glob.glob('image/*.jpg')  # Creates list of names of JPEG files in specified folder
 images = np.array(
@@ -50,63 +50,78 @@ for k in trimmedimages:
 #     si.imsave('normalized/normIMG_{}.jpg'.format(l), img_as_ubyte(segimg[l]))
 print('length of segmented images list: ', len(segimg))
 
-## CREATING ARRAY OF FEATURES FOR IMAGE POV DETECTION ##
-povfeatures = myf.MakePOVfeaturesarray(segimg,len(imgpov))
+## TASK 1, BINARY CLASSIFICATION: PRESENCE/ABSENCE OF TUMOR ##
+t1labels = [x == 'no_tumor' for x in imglabel]      #Creates a separate labels list classifying only presence of tumor
 
-## USING ADABOOST CLASSIFIER TO PREDICT IMAGE POV ##
-# POVLabel = imgpov[:len(imagelist)]  # For if we are not importing all images listed in CSV file
-xtrain, xtest, ytrain, ytest = train_test_split(povfeatures, imgpov, random_state=0)  # Split features & labels
-param_grid = {
-    'n_estimators': range(5, 155, 5),  # Create a dictionary of parameters to try out for optimization
-    'learning_rate': [rate / 10 for rate in range(2, 20, 2)]
-}
-base_model = AdaBoostClassifier(algorithm='SAMME.R', random_state=0)  # Using AdaBoost Classifier with SAMME.R algorithm
-POVmodel = HalvingRandomSearchCV(base_model, param_grid, cv=5, factor=2, n_jobs=-1,
-                                 random_state=0, refit=True).fit(xtrain, ytrain)
-print('Best parameters: ', POVmodel.best_params_)
-print('Best score: ', POVmodel.best_score_)
-ypredict = POVmodel.predict(xtest)
-print('Image POV prediction accuracy: ' + str(accuracy_score(ytest, ypredict)))
-print(classification_report(ytest, ypredict))
+t1numoffeaturerows = 11
+t1numoffeaturecolumns = 9
+t1totalfeatures = (t1numoffeaturerows*200) + (t1numoffeaturecolumns*250)
+t1features = np.zeros(t1totalfeatures)
+for k in segimg[:len(t1labels)]:
+    t1temp = np.concatenate([k[10,:],k[30,:],k[40,:],k[50,:],k[80,:],k[90,:],k[100,:],k[120,:],k[130,:],k[140,:],k[150,:]\
+                     ,k[:,60],k[:,70],k[:,75],k[:,80],k[:,90],k[:,100],k[:,120],k[:,125],k[:,130]])
+    t1features = np.vstack([t1features, t1temp])
+t1features = np.delete(t1features, 0, 0)  # Removes the initialized value at the top of features array
+t1xtrain, t1xtest, t1ytrain, t1ytest = train_test_split(t1features, t1labels, random_state=0)  # Split features & labels
 
-if len(segimg) > len(imgpov):                       # If there are images that do not have labels (i.e. new test images)
-    numofextraimgs = len(segimg) - len(imgpov)      # Use model to predict labels and add them into the label list
-    extraimgs = segimg[-numofextraimgs:]
-    newimgfeatures = myf.MakePOVfeaturesarray(extraimgs,numofextraimgs)
-    newimgPOV = POVmodel.predict(newimgfeatures)
-    imgpov.append(newimgPOV)
 
-## SEPARATE DATASET INTO 3 LISTS ACCORDING TO POV ##
-povsortingmask = np.full(len(segimg), False, dtype=bool)    # Create a mask of boolean values to filter and take
-segimgarray = np.array(segimg)                              # only the values corresponding to required POV
-imglabelarray = np.array(imglabel)
-
-for i in range(0, len(imgpov)):
-    if imgpov[i] == 't':
-        povsortingmask[i] = True
-topviewimg = segimgarray[povsortingmask]
-topviewlabels = imglabelarray[povsortingmask]
-print('Number of top view images: ', len(topviewlabels))
-
-povsortingmask[:] = False
-for i in range(0, len(imgpov)):
-    if imgpov[i] == 's':
-        povsortingmask[i] = True
-sideviewimg = segimgarray[povsortingmask]
-sideviewlabels = imglabelarray[povsortingmask]
-print('Number of side view images: ', len(sideviewlabels))
-
-povsortingmask[:] = False
-for i in range(0, len(imgpov)):
-    if imgpov[i] == 'b':
-        povsortingmask[i] = True
-backviewimg = segimgarray[povsortingmask]
-backviewlabels = imglabelarray[povsortingmask]
-print('Number of back view images: ', len(backviewlabels))
-
-## USING EDGE DETECTION ON ALL TEST IMAGES FOR FEATURE CONTRAST ##
-edgeimg = []
-for i in segimg:
-    edgeimg.append(adjust_gamma(filters.sobel(i), gamma=1)) # Perform sobel edge detection and allow adjustable gamma
+# ## CREATING ARRAY OF FEATURES FOR IMAGE POV DETECTION ##
+# povfeatures = myf.MakePOVfeaturesarray(segimg,len(imgpov))
+#
+# ## USING ADABOOST CLASSIFIER TO PREDICT IMAGE POV ##
+# # POVLabel = imgpov[:len(imagelist)]  # For if we are not importing all images listed in CSV file
+# xtrain, xtest, ytrain, ytest = train_test_split(povfeatures, imgpov, random_state=0)  # Split features & labels
+# param_grid = {
+#     'n_estimators': range(5, 155, 5),  # Create a dictionary of parameters to try out for optimization
+#     'learning_rate': [rate / 10 for rate in range(2, 20, 2)]
+# }
+# base_model = AdaBoostClassifier(algorithm='SAMME.R', random_state=0)  # Using AdaBoost Classifier with SAMME.R algorithm
+# POVmodel = HalvingRandomSearchCV(base_model, param_grid, cv=5, factor=2, n_jobs=-1,
+#                                  random_state=0, refit=True).fit(xtrain, ytrain)
+# print('Best parameters: ', POVmodel.best_params_)
+# print('Best score: ', POVmodel.best_score_)
+# ypredict = POVmodel.predict(xtest)
+# print('Image POV prediction accuracy: ' + str(accuracy_score(ytest, ypredict)))
+# print(classification_report(ytest, ypredict))
+#
+# if len(segimg) > len(imgpov):                       # If there are images that do not have labels (i.e. new test images)
+#     numofextraimgs = len(segimg) - len(imgpov)      # Use model to predict labels and add them into the label list
+#     extraimgs = segimg[-numofextraimgs:]
+#     newimgfeatures = myf.MakePOVfeaturesarray(extraimgs,numofextraimgs)
+#     newimgPOV = POVmodel.predict(newimgfeatures)
+#     imgpov.append(newimgPOV)
+#
+# ## SEPARATE DATASET INTO 3 LISTS ACCORDING TO POV ##
+# povsortingmask = np.full(len(segimg), False, dtype=bool)    # Create a mask of boolean values to filter and take
+# segimgarray = np.array(segimg)                              # only the values corresponding to required POV
+# imglabelarray = np.array(imglabel)
+#
+# for i in range(0, len(imgpov)):
+#     if imgpov[i] == 't':
+#         povsortingmask[i] = True
+# topviewimg = segimgarray[povsortingmask]
+# topviewlabels = imglabelarray[povsortingmask]
+# print('Number of top view images: ', len(topviewlabels))
+#
+# povsortingmask[:] = False
+# for i in range(0, len(imgpov)):
+#     if imgpov[i] == 's':
+#         povsortingmask[i] = True
+# sideviewimg = segimgarray[povsortingmask]
+# sideviewlabels = imglabelarray[povsortingmask]
+# print('Number of side view images: ', len(sideviewlabels))
+#
+# povsortingmask[:] = False
+# for i in range(0, len(imgpov)):
+#     if imgpov[i] == 'b':
+#         povsortingmask[i] = True
+# backviewimg = segimgarray[povsortingmask]
+# backviewlabels = imglabelarray[povsortingmask]
+# print('Number of back view images: ', len(backviewlabels))
+#
+# ## USING EDGE DETECTION ON ALL TEST IMAGES FOR FEATURE CONTRAST ##
+# edgeimg = []
+# for i in segimg:
+#     edgeimg.append(adjust_gamma(feature.canny(i,sigma=1.5), gamma=1)) # Perform sobel edge detection and allow adjustable gamma
 # for j in range(0, len(edgeimg)):
 #     si.imsave('edged/EdgeDetectIMG_{}.jpg'.format(j), img_as_ubyte(edgeimg[j]))
